7e51d8292f1ef7dc14abdfb7b0f1e98ff634cfd3
sylvey <a.few.star@gmail.com>
2026-06-16 13:48:33 -0500

    ui complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import { TextField, Box, Button } from "@mui/material";
+import pkg from "file-uploader-js";
+const FileUploader = pkg.default;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resume, setResume] = useState(null);
+  const [jd, setJd] = useState(null);
+
+  useEffect(() => {
+    if (jd) {
+      console.log("jd:", jd);
+    }
+  }, [jd]);
+
+  const onSubmit = (e) => {};
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+      <Box sx={{ width: "100%" }}>
+        <h3>Upload Your Resume</h3>
+        {resume ? (
+          <div style={{ marginTop: "20px" }}>
+            <p>Selected Resume:</p>
+            <p>
+              {resume.name} {(resume.size / 1024).toFixed(2)} KB
+            </p>
+          </div>
+        ) : (
+          <FileUploader
+            accept={[".pdf"]}
+            uploadedFileCallback={(file) => console.log(file)}
+            renderInput={({ onChange, accept }) => (
+              <div>
+                <button
+                  onClick={() => document.getElementById("fileInput").click()}
+                  style={{
+                    flex: "1",
+                    padding: "10px 20px",
+                    background: "#77b2f11b",
+                    color: "black",
+                    border: "none",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                  }}
+                >
+                  📁 Upload your Resume
+                </button>
+                <input
+                  id="fileInput"
+                  type="file"
+                  accept={accept}
+                  onChange={(e) => {
+                    setResume(e.target.files[0]);
+                  }}
+                  style={{ display: "none" }}
+                />
+              </div>
+            )}
+          />
+        )}
+      </Box>
+      <Box sx={{ width: "100%", marginTop: "20px" }}>
+        <h3>Job Description</h3>
+        <TextField
+          label="Job Description"
+          multiline
+          rows={4}
+          onChange={(e) => {
+            setJd(e.target.value);
+          }}
+          variant="outlined"
+          fullWidth
+          style={{ width: "70%" }}
+        />
+      </Box>
+      <Box sx={{ width: "100%", marginTop: "20px" }}>
+        <Button
+          sx={{
+            width: "90px",
+            background: "hsl(210, 100%, 45%)",
+            color: "white",
+          }}
+          onClick={onSubmit}
         >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          Submit
+        </Button>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
