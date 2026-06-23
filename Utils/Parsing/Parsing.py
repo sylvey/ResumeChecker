1a59b2b7c1368c04efbb65ec6f5afeb103092ab5
sylvey <a.few.star@gmail.com>
2026-06-23 13:08:43 -0500

    environment recreation

--- Utils/Parsing/Parsing.py
+++ Utils/Parsing/Parsing.py
@@ -1,7 +1,7 @@
 import pdfplumber  # pip install pdfplumber
 
 text = ""
-with pdfplumber.open("resume.pdf") as pdf:
+with pdfplumber.open("../../Training_Data/Resume/Bharath_Ganesh_Lead.pdf") as pdf:
     for page in pdf.pages:
         text += page.extract_text() or ""
 print(text)
